1f3fa683ae84ff345c1aed065368e39dd4d424d0
Maxim <max@supervise.ly>
2020-05-27 13:45:35 +0300

    fix mark unknown

--- src/mark_unknown_item.py
+++ src/mark_unknown_item.py
@@ -3,17 +3,8 @@ import supervisely_lib as sly
 import supervisely_lib.io.json as sly_json
 import utils
 
+task_id, api, project_id = utils.get_task_api()
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-task_info = sly_json.load_json_file(os.path.join(SCRIPT_DIR, "../task_config.json"))
-task_id = task_info["task_id"]
-server_address = task_info["server_address"]
-api_token = task_info["api_token"]
-api = sly.Api(server_address, api_token, retry_count=10)
-api.add_additional_field('taskId', task_id)
-api.add_header('x-task-id', str(task_id))
-
-project_id = api.task.get_data(task_id, "state.projectId")
 project_dir = os.path.join(sly.app.SHARED_DATA, "app_tagging", str(project_id))
 image_labels = sly_json.load_json_file(os.path.join(project_dir, "image_labels_pairs.json"))
 free_pairs = sly_json.load_json_file(os.path.join(project_dir, "free_pairs.json"))
@@ -45,6 +36,6 @@ new_label = labels[label_index].add_tags([
 free_pairs.pop(0)
 sly_json.dump_json_file(free_pairs, os.path.join(project_dir, "free_pairs.json"))
 
-utils.get_next_object(api, task_id)
+utils.get_next_object(api, task_id, project_id)
 
 api.task.set_data(task_id, False, "state.tagging")
